05329db4a03c6676826d882c4be7132214c394c8
Austin Shirley <ausshir@gmail.com>
2016-09-27 13:19:56 -0600

    Added initial WBS Mind Map
    Exported from coggle.it

---
+++ Telltale Backup.mm
@@ -0,0 +1,147 @@
+
+<map version="0.9.0">
+    <node TEXT="Telltale" FOLDED="false">
+        <edge COLOR="#b4b4b4" />
+        <font NAME="Helvetica" SIZE="20" />
+        <node TEXT="Product Development" FOLDED="false" POSITION="left">
+            <edge COLOR="#9ed56b" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Construction" FOLDED="false">
+                <edge COLOR="#93d86c" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Design" FOLDED="false">
+                <edge COLOR="#a9d76b" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Selection of Alternatives" FOLDED="false">
+                    <edge COLOR="#beda6e" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+        <node TEXT="Research" FOLDED="false" POSITION="right">
+            <edge COLOR="#67d7c4" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Stress" FOLDED="false">
+                <edge COLOR="#6dd5d9" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="HRV" FOLDED="false">
+                    <edge COLOR="#65d9b2" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="Define HRV" FOLDED="false">
+                        <edge COLOR="#5bd9bd" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Identify HRV Analysis Methods" FOLDED="false">
+                        <edge COLOR="#66daa5" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="HRV Measurement" FOLDED="false">
+                        <edge COLOR="#6adaba" />
+                        <font NAME="Helvetica" SIZE="10" />
+                        <node TEXT="Research SCG Implementation and Characteristics" FOLDED="false">
+                            <edge COLOR="#63dbcc" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                        <node TEXT="Research PPG Implementation and Characteristics" FOLDED="false">
+                            <edge COLOR="#72ded3" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                        <node TEXT="Research ECG  Implementation and Characteristics" FOLDED="false">
+                            <edge COLOR="#6dddb5" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                    </node>
+                </node>
+                <node TEXT="Cause and Effect" FOLDED="false">
+                    <edge COLOR="#76d8da" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="Identify Environmental Factors in Stress" FOLDED="false">
+                        <edge COLOR="#7dddcb" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Identify Diseases Caused by Stress" FOLDED="false">
+                        <edge COLOR="#79dbdd" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Identify Genetic Factors in Stress" FOLDED="false">
+                        <edge COLOR="#79dbd2" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                </node>
+            </node>
+            <node TEXT="Project Environment" FOLDED="false">
+                <edge COLOR="#6edaca" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Problem Statement" FOLDED="false">
+                    <edge COLOR="#74d9d6" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="Identify Background Information" FOLDED="false">
+                        <edge COLOR="#75dadc" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Determine Scope" FOLDED="false">
+                        <edge COLOR="#70d5c4" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Identify Target Market" FOLDED="false">
+                        <edge COLOR="#75dacb" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                </node>
+                <node TEXT="Identify Competitors" FOLDED="false">
+                    <edge COLOR="#77dbd3" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Identify Clients" FOLDED="false">
+                    <edge COLOR="#78dacb" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Pig Anatomy" FOLDED="false">
+                <edge COLOR="#66d4c7" />
+                <font NAME="Helvetica" SIZE="12" />
+                <node TEXT="Research General Pig Anatomy" FOLDED="false">
+                    <edge COLOR="#6bd3d5" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Research Swine Nervous System" FOLDED="false">
+                    <edge COLOR="#66d4b8" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Research Swine Cardiac System" FOLDED="false">
+                    <edge COLOR="#66d6d6" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Research Human-Pig Analogies" FOLDED="false">
+                    <edge COLOR="#6bd7cf" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+        <node TEXT="Documentation and Deliverables" FOLDED="false" POSITION="right">
+            <edge COLOR="#7aa3e5" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Problem" FOLDED="false">
+                <edge COLOR="#75ace2" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Problem Definition" FOLDED="false">
+                    <edge COLOR="#7db1e4" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="Make the Problem Defintion" FOLDED="false">
+                        <edge COLOR="#7bace6" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Write Problem Definition Report" FOLDED="false">
+                        <edge COLOR="#72b3e3" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                </node>
+                <node TEXT="Problem Statement" FOLDED="false">
+                    <edge COLOR="#6dc1e2" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+    </node>
+</map>
